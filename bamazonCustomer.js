3208da7220607666125e389fb27f371388ab5daa
julietshen <julietqshen@gmail.com>
2017-08-07 19:12:46 -0700

    added more code and cleaned up curly braces

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -8,6 +8,7 @@ var app = express();
 var port = 3306;
 
 var connection = mysql.createConnection({
+  post: 3306,
   host: "localhost",
   user: "root",
   password: "",
@@ -16,31 +17,38 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) {
-    console.log("error connecting: " + err.stack)
-    return;
+    throw err;
   }
   console.log("connected at: " + connection.threadId);
   bamazonSearch();
 });
 
 function bamazonSearch() {
-
+  console.log("Selecting all products...\n");
   connection.query('SELECT * FROM products', function(error, results) {
       if (error) throw error;
-      // for (var i = 0; i < results.length; i++) {
-      //   console.log("Item ID: " + results[i].item_id + "\nName: " + results[i].product_name + "\nPrice: " + results[i].price);
-      //   console.log("-------------------");
+      for (var i = 0; i < results.length; i++) {
+        console.log("Item ID: " + results[i].item_id +
+          "\nName: " +
+          results[i].product_name +
+          "\nPrice: " +
+          results[i].price);
+        console.log("-------------------");
         placeOrder();
-      });
+      };
+  });
+
+  placeOrder();
+
   };
+})
 
-  function placeOrder() {
+function placeOrder() {
 
-  inquirer.prompt = ([
-    {
-    type: "input",
-    name: "productID",
-    message: "What is the ID of the product you'd like to order?",
+  inquirer.prompt = ([{
+      type: "input",
+      name: "productID",
+      message: "What is the ID of the product you'd like to order?",
       validate: function(value) {
         if (isNaN(value) === false) {
           return true;
@@ -48,44 +56,54 @@ function bamazonSearch() {
         return false;
       }
     },
-    {
-      type: "input",
-      name: "quantity",
-      message: "How many units would you like to buy?",
-      validate: function(value) {
-        if (isNaN(value) === false) {
-          return true;
+      {
+        type: "input",
+        name: "quantity",
+        message: "How many units would you like to buy?",
+        validate: function(value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
         }
-        return false;
       }
-    }
-  ]).then(function(answers) {
-    var choice;
-    let productID = answers.productID;
-    let quantity = answers.quantity;
-    connection.query('SELECT * FROM products WHERE ?', {item_id: productID}, function(error, results) {
-      var stock = res[0].stock_quantity;
-      let newQuantity = stock - quantity;
-      let price = res[0].price;
+    ]).then(function(answers) {
+      var choice;
+      let productID = answers.productID;
+      let quantity = answers.quantity;
+      connection.query('SELECT stock_quantity FROM products WHERE ?', {
+        item_id: productID
+      }, function(error, results) {
+        var stock = res[0].stock_quantity;
+        let newQuantity = stock - quantity;
+        let price = res[0].price;
 
-      if (error) throw error;
-        for (var i = 0; i < results.length; i++) {
-          if (results[i].item_id === parseInt(answers.productID)) {
-            choice = results[i];
-          };
-        };
-      console.log("There are " + choice.stock_quantity + choice + "left.");
-      if (answers.quantity > choice.stock_quantity) {
-        console.log("Sorry, we are currently out of stock.");
-        bamazonSearch();
-      } else {
-        connection.query('UPDATE product SET ? WHERE ?', function(error, results, fields) {
-              if (error) throw error;
-              console.log("Order successful.");
-              var bamazonTotal = choice.price * parseInt(answers.quantity);
-              console.log("Your total is $" + bamazonTotal)
-          });
-        };
+        if (stock >= quantity) {
+          connection.query("UPDATE products SET ? WHERE ?", [{
+              stock_quantity: totalLeft
+            }, {
+              item_id: id
+            }],
+            function(err, res) {
+              var total = price * quantity;
+              console.log("Your total is $" + total);
+              inquirer.prompt([{
+                type: 'confirm',
+                message: 'Would you like to make another purchase?',
+                name: 'purchase'
+              }]).then(function(answer) {
+                if (answer.purchase === true) {
+                  placeOrder();
+                } else {
+                  return;
+                }
+              });
+            }
+          );
+        } else {
+          console.log("Sorry, we are currently out of stock.");
+          placeOrder();
+        }
       });
     });
   };
